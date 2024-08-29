38f9c614a719e8a5208c5cf8afd69b1abbd6c2dd
Denys Makoviichuk <dmakoviichuk@tenstorrent.com>
2024-08-29 00:27:45 +0000

    testing format

--- tests/hello_test.cpp
+++ tests/hello_test.cpp
@@ -1,7 +1,9 @@
 #include <gtest/gtest.h>
 
 #include "sample/sample_class.hpp"
+
 // Demonstrate some basic assertions.
+
 TEST(HelloTest, AddTest) {
     // Expect two strings not to be equal.
     // Expect equality.
